8f9536501d1620aaf103d9450e432b89a4b799f7
BenBenShang~ <spawpaw@hotmail.com>
2019-05-15 22:34:35 +0800

    initial commit

---
+++ value-filter-core/src/main/java/com/spawpaw/value/filter/core/MappedBy.java
@@ -0,0 +1,15 @@
+package com.spawpaw.value.filter.core;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.ANNOTATION_TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MappedBy {
+    /**
+     * 使用哪个值映射器
+     */
+    Class<? extends ValueMapper> mappedBy();
+}
---
+++ value-filter-core/src/main/java/com/spawpaw/value/filter/core/ValueMapper.java
@@ -0,0 +1,38 @@
+package com.spawpaw.value.filter.core;
+
+import java.lang.annotation.Annotation;
+import java.util.List;
+
+/**
+ * 值映射器。
+ *
+ * @param <T>
+ */
+public interface ValueMapper<T> {
+    /**
+     * 必须的注解
+     *
+     * @return 注解数组
+     */
+    default Class<?>[] requiredAnnotations() {
+        return new Class[]{};
+    }
+
+    /**
+     * 额外支持的注解，字段上没有这些注解也没事
+     *
+     * @return 注解数组
+     */
+    default Class<?>[] externaldAnnotations() {
+        return new Class[]{};
+    }
+
+    /**
+     * 关注：
+     * - 过滤的值是哪种类型
+     * - 过滤的值上有哪些注解, 值所在的类上有哪些注解
+     * - 要进行过滤的值
+     */
+    T map(T value, List<? extends Annotation> annotations);
+
+}
